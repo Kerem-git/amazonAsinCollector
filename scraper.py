b723f383df206f80616cb3b1a90a5f9e3e55e052
Kerem <63135398+Kerem-git@users.noreply.github.com>
2022-05-15 19:15:19 +0300

    Delete scraper.py

--- scraper.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-user_agent = {'User-agent': 'Mozilla/5.0'}
-url ="https://www.amazon.ca/s?me=A2HFA2QNCUMBI8&marketplaceID=A2EUQ1WTGCTBG2" #str(input("Url: "))
-r = requests.get(url,headers=user_agent).text
-
-print(r)
